3deab94c4abe1b1fa514b4e30c39a9a3160dfd71
ayrtonlacerda <38504590+ayrtonlacerda@users.noreply.github.com>
2019-02-26 22:11:31 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1>My TO DOs</h1>
+        </div>
+      </Provider>
     );
   }
 }
